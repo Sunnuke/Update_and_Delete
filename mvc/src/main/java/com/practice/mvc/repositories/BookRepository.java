0bb61e08c0317e02d23d9a237c8e59b991cd357c
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-10 07:53:48 -0800

    Created BookRepository interface, to preset some custom queries, that extends the CrudRepository

---
+++ mvc/src/main/java/com/practice/mvc/repositories/BookRepository.java
@@ -0,0 +1,21 @@
+package com.practice.mvc.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.practice.mvc.models.Book;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+	// this method retrieves all the books from the database
+    List<Book> findAll();
+    
+    // this method finds books with descriptions containing the search string
+    List<Book> findByDescriptionContaining(String search);
+    
+    // this method counts how many titles contain a certain string
+    Long countByTitleContaining(String search);
+    
+    // this method deletes a book that starts with a specific titlecopy
+    Long deleteByTitleStartingWith(String search);
+}
