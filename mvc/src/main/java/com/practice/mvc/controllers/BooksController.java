bbb42509d57c496688270702fa91d1a07bba181e
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-10 18:51:46 -0800

    Created new.jsp to display form for new book input

--- mvc/src/main/java/com/practice/mvc/controllers/BooksController.java
+++ mvc/src/main/java/com/practice/mvc/controllers/BooksController.java
@@ -2,9 +2,14 @@ package com.practice.mvc.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.practice.mvc.models.Book;
 import com.practice.mvc.services.BookService;
@@ -21,5 +26,19 @@ public class BooksController {
         List<Book> books = bookService.allBooks();
         model.addAttribute("books", books);
         return "/books/index.jsp";
+    }
+    
+    @RequestMapping("/books/new")
+    public String newBook(@ModelAttribute("book") Book book) {
+        return "/books/new.jsp";
+    }
+    @RequestMapping(value="/books", method=RequestMethod.POST)
+    public String create(@Valid @ModelAttribute("book") Book book, BindingResult result) {
+        if (result.hasErrors()) {
+            return "/books/new.jsp";
+        } else {
+            bookService.createBook(book);
+            return "redirect:/books";
+        }
     }
 }
